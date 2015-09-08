f29ae9eec47a915a99927f158bec1cc10a99c4f6
riddhi.dilip.vyas <riddhi.dilip.vyas@M5-D-GPNC8R1.dir.svc.accenture.com>
2015-09-08 17:46:58 +0530

    Added test files

--- FileConversionTool/src/main/java/com/acn/file/conversion/tool/gui/TestPage.java
+++ FileConversionTool/src/main/java/com/acn/file/conversion/tool/gui/TestPage.java
@@ -1,6 +1,7 @@
 package com.acn.file.conversion.tool.gui;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
@@ -13,80 +14,79 @@ import javax.swing.SwingConstants;
 
 import com.sun.org.apache.regexp.internal.recompile;
 
-public class TestPage extends JPanel {
-	
-	private static JFrame frame;
+public class TestPage extends JPanel implements ActionListener {
 
-  public TestPage() {
-	  
-	 JLabel fileConversionToolLabel = new JLabel("File Conversion Tool",
+	JFrame frame;
+
+	public TestPage() {
+
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		frame.setResizable(false);
+
+		JLabel fileConversionToolLabel = new JLabel("File Conversion Tool",
 				SwingConstants.CENTER);
-	 
-	 JLabel selectionTypeLabel = new JLabel("Select Converion Type",
+
+		JLabel selectionTypeLabel = new JLabel("Select Converion Type",
 				SwingConstants.CENTER);
-	 
-	  
-    JRadioButton csvToXmlRadio = new JRadioButton("CSV to XML", true);
-    JRadioButton xmlToCsvRadio = new JRadioButton("XML to CSV", false);
-    JRadioButton jsonToCsvRadio = new JRadioButton("Json to CSV", false);
-    
-    JButton submitButton  = new JButton("Submit");
-    
 
-    ButtonGroup radioGroup = new ButtonGroup();
-   
+		JRadioButton csvToXmlRadio = new JRadioButton("CSV to XML", true);
+		JRadioButton xmlToCsvRadio = new JRadioButton("XML to CSV", false);
+		JRadioButton jsonToCsvRadio = new JRadioButton("Json to CSV", false);
 
-    setLayout(null);
+		JButton submitButton = new JButton("Submit");
 
-    add(fileConversionToolLabel);
-    add(selectionTypeLabel);
-    add(csvToXmlRadio);
-    add(xmlToCsvRadio);
-    add(jsonToCsvRadio);
-    add(submitButton);
+		ButtonGroup radioGroup = new ButtonGroup();
 
-    radioGroup.add(csvToXmlRadio);
-    radioGroup.add(xmlToCsvRadio);
-    radioGroup.add(jsonToCsvRadio);
-    
-    Font font = fileConversionToolLabel.getFont();
-    
-    fileConversionToolLabel.setFont(new Font(font.getFontName(), Font.BOLD, 15));
-    
+		setLayout(null);
 
-    fileConversionToolLabel.setBounds(50,10,200,20);
-    selectionTypeLabel.setBounds(20,70,150,20);
-    csvToXmlRadio.setBounds(30, 100, 100, 20);
-    xmlToCsvRadio.setBounds(30, 130, 100, 20);
-    jsonToCsvRadio.setBounds(30, 160, 100, 20);
-    submitButton.setBounds(100,210,80,20);
+		add(fileConversionToolLabel);
+		add(selectionTypeLabel);
+		add(csvToXmlRadio);
+		add(xmlToCsvRadio);
+		add(jsonToCsvRadio);
+		add(submitButton);
 
-    submitButton.addActionListener( new ActionListener() {
-    	public void actionPerformed(java.awt.event.ActionEvent e) {
-    		 
-    		   frame.setVisible(false);
-    		   frame.dispose();
-    		   
-    		   FileConversionEntryPage newFrameObj =  new FileConversionEntryPage();
-    		   newFrameObj.jframe.repaint();
-    		 
-    		  
-    		}
-    });
-    
-    
-  }
+		radioGroup.add(csvToXmlRadio);
+		radioGroup.add(xmlToCsvRadio);
+		radioGroup.add(jsonToCsvRadio);
 
-  public static void main(String[] args) {
-    frame = new JFrame();
-    frame.getContentPane().add(new TestPage());
+		Font font = fileConversionToolLabel.getFont();
 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(300,300);
-    frame.setVisible(true);
-    frame.setResizable(false);
-  }
-  
-  
-  
+		fileConversionToolLabel.setFont(new Font(font.getFontName(), Font.BOLD,
+				15));
+
+		fileConversionToolLabel.setBounds(50, 10, 200, 20);
+		selectionTypeLabel.setBounds(20, 70, 150, 20);
+		csvToXmlRadio.setBounds(30, 100, 100, 20);
+		xmlToCsvRadio.setBounds(30, 130, 100, 20);
+		jsonToCsvRadio.setBounds(30, 160, 100, 20);
+		submitButton.setBounds(100, 210, 80, 20);
+
+		submitButton.addActionListener(this);
+
+	}
+
+	public static void main(String[] args) {
+		TestPage obj = new TestPage();
+		obj.frame.getContentPane().add(obj);
+
+		
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Action Performed = " + e.getSource());
+		this.frame.setVisible(false);
+		this.frame.dispose();
+
+		FileConversionEntryPage newFrameObj = new FileConversionEntryPage();
+		newFrameObj.setVisible(true);
+		//newFrameObj.jframe.repaint();
+
+	}
+
 }
